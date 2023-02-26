ade13b2658e691e4fdcdf62ad756d5538c7fb1b0
moisaovidiu <moisaovidiu94@yahoo.com>
2023-02-26 10:22:22 +0200

    ultimul

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,9 +7,11 @@ import baptism from '../../assets/baptism.webp';
 import baptism2 from '../../assets/baptism2.jpg';
 import Slider from '../../pages/home/Slider';
 import Hero from './Hero';
+import {useNavigate} from 'react-router-dom'
 
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 const Home = ({ imageSrc }) => {
-
+  const navigate = useNavigate()
   return (
     <div className='Home'>
       <Hero imageSrc={wedding2} />
@@ -17,6 +19,16 @@ const Home = ({ imageSrc }) => {
       <Slider imageSrc={wedding3} title={"A successful marriage requires falling in love many times, always with the same person."} subtitle={"Mignon McLaughlin"} flipped={true} />
       <Slider imageSrc={baptism} title={"Baptism is the initial step of a faithful heart."} subtitle={"Max Lucado"}/>
       <Slider imageSrc={baptism2} title={"When we were baptized we took upon us not only the name of Christ, but also the law of obedience."} subtitle={"Robert D. Hales"} flipped={true} />
+    
+      <div className="address">
+          <h1> O~Vis Crafts - shop</h1>
+            <LocationOnIcon fontSize='large'/>
+            <h6>RO 46965700</h6>
+            <p>Porii 198, Floresti / Cluj</p>
+            <p> Romania, 400280</p>
+            <button onClick={()=> navigate ("/contact")}> CONTACT </button>
+        </div>
+    
     </div>
 
 
